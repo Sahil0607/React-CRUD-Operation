8b8cf0f8bc8d372f784d6c30c61236ef7ab07b60
sahilpatel <patel.sahil331@gmail.com>
2025-10-02 13:59:19 -0500

    perfomance optimization

--- src/components/Ingredients/IngredientList.js
+++ src/components/Ingredients/IngredientList.js
@@ -1,26 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import './IngredientList.css';
+import "./IngredientList.css";
 
-const IngredientList = props => {
-  const handleDelete = (id) => {
-    // console.log(id);
-    props.onRemoveItem(id);
-  }
-  
+const IngredientList = React.memo(({ ingredients, onRemoveItem }) => {
   return (
     <section className="ingredient-list">
       <h2>Loaded Ingredients</h2>
       <ul>
-        {props.ingredients.map(ig => (
-          <li key={ig.id} onClick={handleDelete.bind(this, ig.id)}>
-            <span>{ig.title}</span>
-            <span>{ig.amount}x</span>
+        {ingredients.map(({ id, title, amount }) => (
+          <li key={id} onClick={() => onRemoveItem(id)}>
+            <span>{title}</span>
+            <span>{amount}x</span>
           </li>
         ))}
       </ul>
     </section>
   );
-};
+});
 
 export default IngredientList;
